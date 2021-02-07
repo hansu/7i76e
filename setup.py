9f2941050c74cd5486c97f6dcc7f8569515e0b9b
john <bjt128@gmail.com>
2021-02-07 15:21:35 -0600

    add deb

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-# list of (destination, source_file) tuples
-DATA_FILES = [('~/.local/bin', ['m7i76e/mesaflash64', 'm7i76e/mesaflash32',])]
-data_files = [(os.path.expanduser(dest), src_list) for dest, src_list in DATA_FILES]
-
 setup(
-    name="7i76e",
-    version="0.0.5",
+    name="c7i76e",
+    version="0.1.5",
     author="John Thornton",
     author_email="<jt@gnipsel.com>",
     description="Mesa configuration tool for 7i76e",
@@ -19,11 +15,20 @@ setup(
     url="https://github.com/jethornton/7i76e",
     download_url="https://github.com/jethornton/7i76e/tarball/master",
     python_requires='>=3',
-    packages=find_packages(),
-    data_files=data_files,
+    platforms=['Posix'],
+    packages=['c7i76e'],
     include_package_data=True,
     entry_points={
-        'gui_scripts': ['7i76e=m7i76e.m7i76e:main',],
+        'gui_scripts': ['c7i76e=c7i76e.c7i76e:main',],
     },
+    data_files = [
+        ('share/applications/', ['7i76E Configurator.desktop'])
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Environment :: X11 Applications :: Qt",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+    ],
 )
-
