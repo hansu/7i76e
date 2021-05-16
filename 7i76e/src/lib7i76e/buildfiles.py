e18590c647653b30720dd9f6c6286bd90626325f
john <bjt128@gmail.com>
2021-05-15 19:16:08 -0500

    build directories if they don't exist

---
+++ 7i76e/src/lib7i76e/buildfiles.py
@@ -0,0 +1,20 @@
+import os, traceback
+
+def build(parent): # check for linuxcnc paths
+	if not os.path.exists(os.path.expanduser('~/linuxcnc/configs')):
+		try:
+			os.mkdir(os.path.expanduser('~/linuxcnc/configs'))
+		except OSError:
+			parent.outputPTE.appendPlainText(f'OS error\n {traceback.print_exc()}')
+
+	if not os.path.exists(os.path.expanduser('~/linuxcnc/nc_files')):
+		try:
+			os.mkdir(os.path.expanduser('~/linuxcnc/nc_files'))
+		except OSError:
+			parent.outputPTE.appendPlainText(f'OS error\n {traceback.print_exc()}')
+
+	if not os.path.exists(os.path.expanduser('~/linuxcnc/subroutines')):
+		try:
+			os.mkdir(os.path.expanduser('~/linuxcnc/subroutines'))
+		except OSError:
+			parent.outputPTE.appendPlainText(f'OS error\n {traceback.print_exc()}')
