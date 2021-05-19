33300021fab1692c9a7c371d104a42d14ae64d94
john <bjt128@gmail.com>
2021-05-19 06:21:29 -0500

    fix create dir from fresh install

--- 7i76e/src/lib7i76e/buildfiles.py
+++ 7i76e/src/lib7i76e/buildfiles.py
@@ -1,6 +1,12 @@
 import os, traceback
 
 def build(parent): # check for linuxcnc paths
+	if not os.path.exists(os.path.expanduser('~/linuxcnc')):
+		try:
+			os.mkdir(os.path.expanduser('~/linuxcnc'))
+		except OSError:
+			parent.outputPTE.appendPlainText(f'OS error\n {traceback.print_exc()}')
+
 	if not os.path.exists(os.path.expanduser('~/linuxcnc/configs')):
 		try:
 			os.mkdir(os.path.expanduser('~/linuxcnc/configs'))
